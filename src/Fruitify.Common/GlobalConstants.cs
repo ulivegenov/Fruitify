7548673e3ffbfa87096522f0bcc38dc13d5d861a
ulivegenov <uli_vegenov@abv.bg>
2020-05-31 19:08:31 +0300

    1/ Scaffold Register and Login pages. 2/ Modified Register and Login pages.

--- src/Fruitify.Common/GlobalConstants.cs
+++ src/Fruitify.Common/GlobalConstants.cs
@@ -2,10 +2,14 @@
 {
     public static class GlobalConstants
     {
+        // System
         public const string SystemName = "Fruitify";
 
+        // Roles
         public const string AdministratorRoleName = "Administrator";
-
         public const string UserRoleName = "User";
+
+        // Error Messages
+        public const string CharactersCountMessage = "The {0} must be at least {2} and at max {1} characters long.";
     }
 }
---
+++ src/Web/Fruitify.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Fruitify.Data;
+using Fruitify.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Fruitify.Web.Areas.Identity.IdentityHostingStartup))]
+namespace Fruitify.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
