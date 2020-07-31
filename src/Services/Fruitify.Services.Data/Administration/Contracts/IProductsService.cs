45ce9ff56d3063df70aaebeb7a6d06ef470413d2
ulivegenov <uli_vegenov@abv.bg>
2020-07-31 10:52:50 +0300

    1/ Implement GetAllWeekProducts method in src/Services/Fruitify.Services.Data/Administration/ProductsService.cs 2/ Implement GetDayProduct method in src/Services/Fruitify.Services.Data/Administration/ProductsService.cs

--- src/Services/Fruitify.Services.Data/Administration/Contracts/IProductsService.cs
+++ src/Services/Fruitify.Services.Data/Administration/Contracts/IProductsService.cs
@@ -1,8 +1,14 @@
 ﻿namespace Fruitify.Services.Data.Administration.Contracts
 {
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
     using Fruitify.Services.Data.Base.Contarcts;
 
     public interface IProductsService : IBaseService<int>
     {
+        Task<IEnumerable<T>> GetAllWeekProducts<T>();
+
+        Task<T> GetDayProduct<T>();
     }
 }
--- src/Services/Fruitify.Services.Data/Administration/ProductsService.cs
+++ src/Services/Fruitify.Services.Data/Administration/ProductsService.cs
@@ -1,9 +1,16 @@
 ﻿namespace Fruitify.Services.Data.Administration
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Fruitify.Common;
     using Fruitify.Data.Common.Repositories;
     using Fruitify.Data.Models;
     using Fruitify.Services.Data.Administration.Contracts;
     using Fruitify.Services.Data.Base;
+    using Fruitify.Services.Mapping;
+    using Microsoft.EntityFrameworkCore;
 
     public class ProductsService : BaseService<Product, int>, IProductsService
     {
@@ -13,6 +20,27 @@
             : base(productsRepository)
         {
             this.productsRepository = productsRepository;
+        }
+
+        public async Task<IEnumerable<T>> GetAllWeekProducts<T>()
+        {
+            var weekProducts = await this.productsRepository.All()
+                                                            .Where(p => p.WeekProduct == true)
+                                                            .To<T>()
+                                                            .Take(GlobalConstants.WeekProductsCount)
+                                                            .ToListAsync();
+
+            return weekProducts;
+        }
+
+        public async Task<T> GetDayProduct<T>()
+        {
+            var dayProduct = await this.productsRepository.All()
+                                                          .Where(p => p.DayProduct == true)
+                                                          .To<T>()
+                                                          .FirstOrDefaultAsync();
+
+            return dayProduct;
         }
     }
 }
